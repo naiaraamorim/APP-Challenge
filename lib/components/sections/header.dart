3fd088767cc0c0ce551c14d904efc64a4e5b1f73
Naiara Amorim <naiarataianelimaamorim@gmail.com>
2024-03-22 21:50:29 -0300

    build: adicionando estilo

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -11,18 +11,57 @@ class _HeaderState extends State<Header> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('\$ 1000.00'),
-              Text('Balanço Disponível'),
-            ],
-          ),
-          Icon(Icons.account_circle)
-        ],
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+              Colors.lightGreen,
+              Colors.green,
+
+              // Color.fromRGBO(103, 99, 234, 1),
+              // Color.fromRGBO(155, 105, 254, 1),
+              // Color.fromRGBO(195, 107, 255, 1),
+            ]),
+        //color: Color.fromARGB(255, 77, 141, 69),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(25)),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.fromLTRB(16, 40, 16, 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // Text(
+                //   '\$ 1000.00',
+                //   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                // ),
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(fontSize: 18),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: '1000.00',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ),
+                Text(
+                  'Balanço Disponível',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 50,
+            )
+          ],
+        ),
       ),
     );
   }
